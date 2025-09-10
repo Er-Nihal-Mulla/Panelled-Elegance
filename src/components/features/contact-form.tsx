19fe04eddcd9c6c84acdf97113a0d5c20677bbbf
Nihal Mulla <nihalmulla9784@gmail.com>
2025-09-10 15:45:38 +0000

    address corrected

--- src/components/features/contact-form.tsx
+++ src/components/features/contact-form.tsx
@@ -16,15 +16,15 @@ export function ContactForm() {
         <div className="flex flex-col items-center space-y-4 pt-4">
           <div className="flex items-center gap-4">
             <Phone className="h-5 w-5 text-primary" />
-            <a href="tel:+919284224763" className="text-lg hover:underline">+91 92842 24763</a>
+            <a href="tel:+919284224763" className="text-lg hover:underline">+91 7841926727</a>
           </div>
           <div className="flex items-center gap-4">
             <Mail className="h-5 w-5 text-primary" />
-            <a href="mailto:contact@b4paneling.com" className="text-lg hover:underline">contact@b4paneling.com</a>
+            <a href="mailto:contact@b4paneling.com" className="text-lg hover:underline">b4paneling@gmail.com</a>
           </div>
           <div className="flex items-center gap-4">
             <MapPin className="h-5 w-5 text-primary" />
-            <p className="text-lg">123 Panelling Drive, Design District, 400051</p>
+            <p className="text-lg">Shop No. 2, near inam masjid, Kabnur Ichalkaranji, Maharashtra, 416116</p>
           </div>
         </div>
       </CardContent>
